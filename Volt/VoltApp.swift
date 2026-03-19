0ab06e1662c4faaff86a2b72e467e2fbf320c33a
Salman Qureshi <er.salmanqureshi@gmail.com>
2026-03-19 09:23:54 +0400

    Initial Commit

---
+++ Volt/VoltApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoltApp.swift
+//  Volt
+//
+//  Created by Salman Qureshi on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct VoltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
